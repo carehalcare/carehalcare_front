e1c6d60c0fd311850c058d0a729eed25fc3df3d0
yenidanan <yenidanan@sookmyung.ac.kr>
2023-05-29 18:30:53 +0900

    출퇴근 nfc 기능 구현

---
+++ app/src/main/java/carehalcare/carehalcare/Feature_NFC/CommuteResponseDto.java
@@ -0,0 +1,67 @@
+package carehalcare.carehalcare.Feature_NFC;
+
+public class CommuteResponseDto {
+    private Long id;
+    private String userId;
+    private String puserId;
+    private String category;
+    private String date;
+    private String time;
+    private String createdDateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPuserId() {
+        return puserId;
+    }
+
+    public void setPuserId(String puserId) {
+        this.puserId = puserId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(String createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+}
